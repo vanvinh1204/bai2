1d39506b07cb75d48d11d2c41fb486fff433ddf3
Nguyen Van Vinh <v.vinh72266@yev.vn>
2017-12-18 13:59:25 +0700

    Initial commitaaa

---
+++ app/src/main/java/com/example/vvinh72266/bai2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vvinh72266.bai2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
